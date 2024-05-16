b791c987d13c1946e3f1bd193a2d7c738b91bd50
Abhay vishwakarma <142080096+Abhay7111@users.noreply.github.com>
2024-05-16 08:49:59 +0530

    fd

--- src/components/maindisplay.jsx
+++ src/components/maindisplay.jsx
@@ -6,7 +6,7 @@ function Maindisplay() {
     <div className='bg-red-400 w-full'>
      <Displaynav/>
      <div className='w-full h-[93.9vh] bg-green-400 overflow-hidden'>
-
+      hii
      </div>
      </div>
   )
